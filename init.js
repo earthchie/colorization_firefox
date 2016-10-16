62d88b2cf32cda81cc9ac516f73da6c1aebf0c11
earthchie <earthchie@gmail.com>
2016-10-16 21:29:43 +0700

    first commit

---
+++ init.js
@@ -0,0 +1,8 @@
+var css = document.getElementById('extension_colorize');
+if(!css){
+    css = document.createElement("style");
+    css.id = "extension_colorize";
+    document.head.appendChild(css);
+}
+css.type = "text/css";
+css.innerHTML = "html * { filter: initial !important; }";
